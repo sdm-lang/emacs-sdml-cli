f3f11f040a5782e82e51191be0e1d658b67b0bce
Simon Johnston <johnstonskj@gmail.com>
2025-10-31 07:18:24 -0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,159 @@
+# SDML Mode for Emacs
+
+![SDML Logo Text](https://raw.githubusercontent.com/sdm-lang/.github/main/profile/horizontal-text.svg)
+
+This package provides an Emacs tree-sitter based major mode for SDML - the
+[Simple Domain Modeling Language](https://github.com/sdm-lang/tree-sitter-sdml).
+
+[![License-Apache_2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Eask Workflow](https://github.com/sdm-lang/emacs-sdml-ts-mode/actions/workflows/emacs-eask.yml/badge.svg)](https://github.com/sdm-lang/emacs-sdml-ts-mode/actions/workflows/emacs-eask.yml)
+[![MELPA](https://melpa.org/packages/sdml-ts-mode-badge.svg)](https://melpa.org/#/sdml-ts-mode)
+[![Stargazer Count](https://img.shields.io/github/stars/sdm-lang/emacs-sdml-ts-mode.svg)](https://github.com/sdm-lang/emacs-sdml-ts-mode/stargazers)
+
+## Installing
+
+Install is easiest from MELPA, here's how with `use-package`.
+
+```elisp
+(use-package sdml-ts-mode)
+;; or
+(use-package sdml-ts-mode
+  :vc (:url "https://github.com/sdm-lang/emacs-sdml-ts-mode"))
+```
+
+Or, interactively; `M-x package-install RET sdml-ts-mode RET`
+
+## Usage
+
+Once installed the major mode should be used for any file ending in `.sdm` or
+`.sdml` with highlighting and indentation support.
+
+## Ctags Support
+
+Using [Universal Ctags](https://ctags.io) and the [sdml-ctags](https://github.com/sdm-lang/sdml-ctags) package provides an identifier tagging
+solution for SDML source. The `sdml-ts-mode-ctags-mode` will determine if
+[`company-mode`](https://company-mode.github.io/) installed and add SDML as a supported tag backend. Additionally,
+this minor mode provides a command to re-create the project's tag file. The
+image below shows company used as the completion UI for type completion when
+editing.
+
+![Completion](./images/emacs-completion.png)
+
+Command `sdml-ts-mode-ctags-generate` has the default binding `C-c C-s T`. It uses
+the variables `sdml-ts-mode-ctags-command` and `sdml-ts-mode-ctags-output-file-name`
+to generate the tag file.
+
+## Tool Commands
+
+A number of the tools provided by the SDML command-line tool are exposed as
+Emacs commands. The following image shows two tools in use, the module
+dependency tree and the validation tool.
+
+![Tool Commands](./images/emacs-tools.png)
+
+### Dependency Tree
+
+* Command `sdml-ts-mode-current-buffer-dependency-tree` has the default binding
+  `C-c C-s t`.
+* The command will prompt for the maximum depth of the tree where 0 means
+  unbounded. This is a command prefix and can therefore be specified with the
+  usual `C-u` binding.
+* The resulting tree view can be refreshed using the common binding of `g` and
+  quit with `q`.
+  
+### Dependency Graph
+
+If running under a `window-sytem` it is also possible to display the current
+buffer's dependencies as a directed graph. The tool will generate an SVG and
+display in a read-only window.
+
+* Command `sdml-ts-mode-current-buffer-dependency-graph` has the default binding
+  `C-c C-s M-t`.
+
+### Full Validation
+
+* Command `sdml-ts-mode-validate-current-buffer` has the default binding `C-c C-s
+  v`.
+* Command `sdml-ts-mode-validate-file` has the default binding `C-c C-s M-v`.
+* The variable `sdml-ts-mode-validation-level` denotes the level of messages
+  produced by the validator, with a default of `warnings`.
+* The output uses the standard `compilation-mode` with all the common bindings are
+  available.
+
+### Abbreviations and Skeletons
+
+TBD
+
+## Prettify Symbol
+
+TBD
+
+### Default Key Bindings
+
+TBD
+
+## Add-Ons
+
+TBD
+
+## License(s)
+
+The contents of this repository are made available under the following
+licenses:
+
+### Apache-2.0
+
+> ```text
+> Copyright 2025 Simon Johnston <johnstonskj@gmail.com>
+> 
+> Licensed under the Apache License, Version 2.0 (the "License");
+> you may not use this file except in compliance with the License.
+> You may obtain a copy of the License at
+> 
+>     http://www.apache.org/licenses/LICENSE-2.0
+> 
+> Unless required by applicable law or agreed to in writing, software
+> distributed under the License is distributed on an "AS IS" BASIS,
+> WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+> See the License for the specific language governing permissions and
+> limitations under the License.
+> ```
+
+See the enclosed file [LICENSE-Apache](https://github.com/sdm-lang/emacs-sdml-ts-mode/blob/main/LICENSE-APACHE).
+
+### MIT
+
+> ```text
+> Copyright 2025 Simon Johnston <johnstonskj@gmail.com>
+> 
+> Permission is hereby granted, free of charge, to any person obtaining a copy
+> of this software and associated documentation files (the “Software”), to deal
+> in the Software without restriction, including without limitation the rights to
+> use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+> the Software, and to permit persons to whom the Software is furnished to do so,
+> subject to the following conditions:
+> 
+> The above copyright notice and this permission notice shall be included in all
+> copies or substantial portions of the Software.
+> 
+> THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+> INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+> PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+> HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+> OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+> SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+> ```
+
+See the enclosed file [LICENSE-MIT](https://github.com/sdm-lang/emacs-sdml-ts-mode/blob/main/LICENSE-MIT).
+
+### Contributions
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
+
+## Changes
+
+### Version 0.1.0
+
+Initial release.
---
+++ sdml-cli.el
@@ -0,0 +1,176 @@
+;;; sdml-cli.el --- CLI integration -*- lexical-binding: t; -*-
+
+;; Author: Simon Johnston <johnstonskj@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "29.1"))
+;; URL: https://github.com/sdm-lang/emacs-sdml-cli
+;; Keywords: languages tools
+
+;;; License:
+
+;; Copyright (c) 2025 Simon Johnston
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;     http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+;; Copyright 2025 Simon Johnston <johnstonskj@gmail.com>
+;; 
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the “Software”), to deal
+;; in the Software without restriction, including without limitation the rights to
+;; use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+;; the Software, and to permit persons to whom the Software is furnished to do so,
+;; subject to the following conditions:
+;; 
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+;; 
+;; THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+;; INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+;; PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+;;; Commentary:
+
+;; his package provides for a wrapper for the SDML command-line tool.
+
+;;; Code:
+
+(require 'ansi-color) ;; built-in
+(require 'seq)
+
+;; --------------------------------------------------------------------------
+;; Customization
+;; --------------------------------------------------------------------------
+
+(defcustom sdml-cli-name "sdml"
+  "The name of the command line tool to use for SDML actions."
+  :tag "CLI tool name"
+  :type 'file
+  :group 'sdml)
+
+(defcustom sdml-cli-log-filter 'none
+  "The level of log information to emit from the command line tool."
+  :tag "Logging filter level"
+  :type '(choice
+	  (const :tag "None" none)
+	  (const :tag "Errors" errors)
+	  (const :tag "Warnings" warnings)
+	  (const :tag "Information" information)
+	  (const :tag "Debugging" debugging)
+	  (const :tag "Tracing" tracing))
+  :group 'sdml)
+
+(defcustom sdml-cli-load-path nil
+  "A list of directories to be used as the `SDML_PATH' variable value."
+  :tag "CLI load path"
+  :type '(repeat directory)
+  :group 'sdml)
+
+(defcustom sdml-cli-no-color nil
+  "Suppress color output from the command line tool."
+  :tag "Suppress color"
+  :type 'boolean
+  :group 'sdml)
+
+;; --------------------------------------------------------------------------
+;; Section heading
+;; --------------------------------------------------------------------------
+
+(defconst sdml-cli-default-output-buffer-name "*SDML Command-Line Output*")
+
+(defconst sdml-cli-default-error-buffer-name "*SDML Command-Line Errors*")
+
+(defun sdml-cli-make-arg (name value)
+  "Make an argument string from NAME and VALUE."
+  (format "--%s %s" name value))
+
+(defun sdml-cli-make-command (command &rest args)
+  "Make an sdml COMMAND with additional ARGS."
+  (when (or t (derived-mode-p 'sdml-mode))
+    (let* ((cli-name (or sdml-cli-name "sdml"))
+           (cmd-name (executable-find cli-name))
+           (pre-args
+	    (list
+             cmd-name
+             (sdml-cli-make-arg 'log-filter sdml-cli-log-filter)
+             (if sdml-cli-no-color "--no-color" nil)
+             command))
+           (args
+	    (mapcar
+	     (lambda (arg)
+	       (cond
+		((eq arg 'current-buffer)
+		 (sdml-cli-make-arg 'input (buffer-file-name)))
+		(t arg)))
+             args)))
+      (cond
+       ((null cmd-name)
+        (message "couldn't find the sdml cli: %s" cli-name))
+       (t (string-join (append pre-args args) " "))))))
+
+(defun sdml-cli--make-refresh-cmd (cmd env out err)
+  "Return a lambda to refresh the output buffer from a command.
+CMD is the command-line to run, ENV is the environment variables
+to add, OUT is the output buffer and ERR the error buffer."
+  (lambda ()
+    (interactive)
+    (setq buffer-read-only nil)
+    (delete-region (point-min) (point-max))
+    (with-environment-variables
+	(("SDML_PATH" env))
+      (shell-command cmd out err)
+      ;; colorize output
+      (ansi-color-apply-on-region (point-min) (point-max))
+      (setq buffer-read-only t))))
+
+(defun sdml-cli-run-command (command &optional output-buffer-name error-buffer-name refresh-fn)
+  "Run COMMAND with output to OUTPUT-BUFFER-NAME and ERROR-BUFFER-NAME.
+
+If not specified OUTPUT-BUFFER-NAME is set to
+`sdml-cli-default-output-buffer-name' and ERROR-BUFFER-NAME is set
+to `sdml-cli-default-error-buffer-name'.
+
+The boolean REFRESH-FN indicates that a refresh function should
+be added to the buffer with a key binding to \"g\"."
+  (let ((is-special (null output-buffer-name))
+	(output-buffer-name
+	 (or output-buffer-name sdml-cli-default-output-buffer-name))
+	(load-path
+	 (concat
+	  (or (getenv "SDML_PATH") "")
+          (string-join sdml-cli-load-path ":"))))
+    (with-environment-variables
+	(("SDML_PATH" load-path))
+      (shell-command command
+                     output-buffer-name
+                     (or error-buffer-name sdml-cli-default-error-buffer-name))
+      (when is-special
+        (pop-to-buffer output-buffer-name)
+        ;; colorize output
+        (ansi-color-apply-on-region (point-min) (point-max))
+        ;; make read-only
+        (special-mode)
+        (when refresh-fn
+          ;; install refresh command
+          (use-local-map (copy-keymap special-mode-map))
+          (local-set-key "g"
+			 (sdml-cli--make-refresh-cmd command
+                                                             load-path
+                                                             output-buffer-name
+                                                             error-buffer-name)))))))
+
+(provide 'sdml-cli)
+
+;;; sdml-cli.el ends here
